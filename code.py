c594656d0335ddc266b587dffd02e333d4082b22
crapWizard <hairlesschickens@gmail.com>
2017-02-01 20:57:07 +1100

    Add files via upload
    yeahhhhhh boiiiiiiiiiiiiiiiiiiiiiiiiii

--- code.py
+++ code.py
@@ -1,54 +1,108 @@
 # this a text adventure based upon the book 'the name of the wind' by patrick rothfuss
-# by Cecily and Casey
+# by cecily and casey
 # 2016
 
 import random
+from Tkinter import *
+
 
 class Functions:
 
-    def fnInput(self):
-
+    def __init__(self):
         self.fnSetBaseStats()
 
+    def fnInput(self,i):
+        playerInput = appBasicGUI.entCmd.get()
+
+        # print whatever was typed onto the game screen
+        appBasicGUI.txtFrame.insert(INSERT,playerInput + '\n')
+        # scrolls to newest entry on box
+        appBasicGUI.txtFrame.see(END)
+
+
+        appBasicGUI.entCmd.delete(0, 'end') # clear entry box
+
         while True:
-            playerInput = raw_input(": ")
-
-
+            # update base stats display
+            appBasicGUI.lblBaseStats.config(text="\n" + 'Energy:  ' + str(functions.energy) + "\n" + "\n" +
+                                          'Lute Skill:  ' + str(functions.luteSkill) + "\n" + "\n"
+                                                                                              'Charisma:  ' + str(
+                functions.charisma) + "\n" + "\n" +
+                                          'Appearance:  ' + str(functions.appearance) + "\n" + "\n" +
+                                          'Rank:  ' + str(functions.rank))
             # cardinal direction move
 
             # North (up)
             if playerInput == "n":
-                if self.location[1] > 0:
+                if self.location[1] > 0 and self.location[0] == 1:
                     self.location[1] -= 1
+                    self.energy -= 3
+                    appBasicGUI.txtFrame.insert(INSERT, "You went North" + '\n')
+                    self.fnCheckLocation()
+                    break
                 else:
-                    print "There is a barrier, you cannot go in this direction"
+                    appBasicGUI.txtFrame.insert(INSERT,"There is a barrier, you cannot go in this direction" + '\n')
+                    break
             # South (down)
             elif playerInput == "s":
-                if self.location[1] < 2 or self.location[0] == 1:  # staircase exception
+                if self.location[1] < 2  and self.location[0] == 1 and self.location[0] == 1:
                     self.location[1] += 1
+                    self.energy -= 3
+                    self.fnCheckLocation()
+                    appBasicGUI.txtFrame.insert(INSERT, "You went South" + '\n')
+                    break
+                # staircase exception
+                elif self.location == [1,2,0]:
                     # if self.location[0] == 1, then they are on the staircase and need to be taken to the next level
+                    if self.rank == 'relar':
+                        self.fnRankRelar
+                    elif self.rank == 'elthe':
+                        self.fnRnakElthe
+                        break
+                    else:
+                        appBasicGUI.txtFrame.insert(INSERT, "You enter the staircase but encouter a barrier in form of a chain over the bottom most step. "
+                                                            "Try coming back when you reach a better rank" + '\n')
+                        break
+
                 else:
-                    print "There is a barrier, you cannot go in this direction"
+                    appBasicGUI.txtFrame.insert(INSERT, "There is a barrier, you cannot go in this direction" + '\n')
+                    break
             # East (right)
             elif playerInput == "e":
                 if self.location[0] < 2:
                     self.location[0] += 1
+                    self.energy -= 3
+                    self.fnCheckLocation()
+                    appBasicGUI.txtFrame.insert(INSERT, "You went East" + '\n')
+                    break
                 else:
-                    print "There is a barrier, you cannot go in this direction"
+                    appBasicGUI.txtFrame.insert(INSERT, "There is a barrier, you cannot go in this direction" + '\n')
+                    break
             # West (left)
             elif playerInput == "w":
                 if self.location[0] > 0:
                     self.location[0] -= 1
+                    self.energy -= 3
+                    self.fnCheckLocation()
+                    appBasicGUI.txtFrame.insert(INSERT, "You went West" + '\n')
+                    break
                 else:
-                    print "There is a barrier, you cannot go in this direction"
+                    appBasicGUI.txtFrame.insert(INSERT, "There is a barrier, you cannot go in this direction" + '\n')
+                    break
 
 
             # a map is displayed
             elif playerInput == "map":
                 self.fnMaps()
+                break
+
             else:
-                print "Sorry, that command was not recognised"
-            print self.location
+                appBasicGUI.txtFrame.insert(INSERT, "Sorry, that command was not recognised" + '\n')
+                break
+
+
+
+
 
 
 
@@ -169,7 +223,7 @@ class Functions:
                 | masters  ||   || library          |
                 | room     ||   ||                  |
                 -----------||   ||-------------------
-                       | stairwell |
+                       | stairwell |0
                        |           |
                        -------------
 
@@ -297,30 +351,36 @@ class Functions:
                 W --|-- E
                     S"""
         if self.location == [0,0,0]:
-            print self.uni000
+            appBasicGUI.txtFrame.insert(INSERT, self.uni000 + '\n')
         elif self.location == [0,1,0]:
-            print self.uni010
+            appBasicGUI.txtFrame.insert(INSERT,self.uni010+ '\n')
         elif self.location == [2,0,0]:
-            print self.uni200
+            appBasicGUI.txtFrame.insert(INSERT,self.uni200+ '\n')
         elif self.location == [0,2,0]:
-            print self.uni020
+            appBasicGUI.txtFrame.insert(INSERT,self.uni020+ '\n')
         elif self.location == [1,0,0]:
-            print self.uni100
+            appBasicGUI.txtFrame.insert(INSERT,self.uni100+ '\n')
         elif self.location == [1,1,0]:
-            print self.uni110
+            appBasicGUI.txtFrame.insert(INSERT,self.uni110+ '\n')
         elif self.location == [1,2,0]:
-            print self.uni120
+            appBasicGUI.txtFrame.insert(INSERT,self.uni120+ '\n')
         elif self.location == [2,2,0]:
-            print self.uni220
+            appBasicGUI.txtFrame.insert(INSERT,self.uni220+ '\n')
         elif self.location == [2,1,0]:
-            print self.uni210
+            appBasicGUI.txtFrame.insert(INSERT,self.uni210+ '\n')
+        # scrolls to newest entry in box
+        appBasicGUI.txtFrame.see(END)
 
 
 
 
     def fnSetBaseStats(self):
 
-        # set initial location
+        # chat dialogue for convos throughout the game
+
+        self.chatResponse = []
+
+    # set initial location
         self.location = [0,0,0]
 
         """ these are the following base stats initially generated at the start of the game and
@@ -339,11 +399,33 @@ class Functions:
         # set initital rank
         self.rank = 'elir'
 
-        print 'energy', self.energy
-        print 'Lute Skill', self.luteSkill
-        print 'Charisma', self.charisma
-        print 'Appearance', self.appearance
-        print 'Rank', self.rank
+    def fnDormitory(self):
+        appBasicGUI.txtFrame.insert(INSERT, "You are now in the dormitory.\n")
+        appBasicGUI.txtFrame.insert(INSERT, "Rows of spacious single beds line the walls. A small desk is allocated for each bunk. Bed furnishings are modest, yet comfortable.\n")
+        self.energy = 100
+        appBasicGUI.txtFrame.insert(INSERT,"Energy fully restored, you may now exit the dormitory\n")
+        # scrolls to newest entry in box
+        appBasicGUI.txtFrame.see(END)
+
+    def fnMessHall(self):
+        print "You are now in the mess hall"
+        response = input('Would you like to eat or chat?')
+        if response == 'eat':
+            self.energy += 10
+        elif response == 'chat':
+            print random.choice(self.chatDialogue)
+            self.charisma += 1
+            input = input("What do you think?")
+            print random.choice(self.chatResponse)
+
+
+
+    def fnCheckLocation(self):
+        if self.location == [0,0,0]:
+            self.fnDormitory()
+        elif self.location == [2,2,0]:
+            self.fnMessHall()
+
 
     def fnSetSubjectStats(self):
 
@@ -355,12 +437,72 @@ class Functions:
         if self.rank == 'elir':
             for eachSubject in self.elirSubjects:
                 self.elirSubjects[eachSubject] = random.randrange(0,40)
-        elif self.rank == 'relir':
+        elif self.rank == 'relar':
             for eachSubject in self.relarSubjects:
                 self.relarSubjects[eachSubject] = random.randrange(0,40)
         if self.rank == 'elthe':
             for eachSubject in self.eltheSubjects:
                 self.eltheSubjects[eachSubject] = random.randrange(0,40)
         print self.elirSubjects
+
+
+
+class GUI:
+
+    # set inital gui stuff
+    def __init__(self, master):
+        self.master = master
+        self.master.title("The Academy")
+        self.master.minsize(700, 550)
+        self.master.maxsize(700, 550)
+        self.fnCreateWidgets()
+
+
+
+
+    def fnCreateWidgets(self):
+        self.strCmd = StringVar()
+
+
+        self.frWindow = Frame(height=400, width=500)
+        self.frWindow.pack(fill=BOTH, expand=1)
+
+        self.txtFrame = Text(self.frWindow, relief = SUNKEN, height = 30, width = 80)
+        self.txtFrame.grid(row = 0, column = 0)
+        self.txtFrame.insert(INSERT, "Welcome to the Academy!\n")
+
+        # label to display all base stats
+        self.lblBaseStats = Label(self.frWindow, relief = SUNKEN, anchor = N, height = 30, width =16, bg = 'light grey', font  ='MONACO 12 bold' )
+        self.lblBaseStats.grid(row = 0, column = 2, padx = 5)
+
+
+        # display stats
+        self.lblBaseStats.config(text =  "\n" +'Energy:  ' + str(functions.energy) + "\n"+ "\n"+
+        'Lute Skill:  '+ str(functions.luteSkill) + "\n" + "\n"
+        'Charisma:  ' + str(functions.charisma) + "\n"+  "\n" +
+        'Appearance:  ' + str(functions.appearance) + "\n" + "\n"+
+        'Rank:  '+ str(functions.rank))
+
+        # entry box for control of character
+        self.entCmd = Entry(self.frWindow, relief = SUNKEN, width = 80)
+        self.entCmd.grid(row = 1 , column = 0, pady = 10)
+
+        # button, enters text in entry box- also binds return key to entry
+        self.btnEntry = Button(self.frWindow,relief = RAISED,text = 'enter', width = 14, height = 1, command = lambda :functions.fnInput(0))
+        self.btnEntry.grid(row = 1, column = 2, pady = 10)
+        self.entCmd.bind('<Return>', functions.fnInput)
+
+
+
+
+
+
 functions = Functions()
-Functions.fnInput(functions)
+
+wdBaseWindow = Tk()
+appBasicGUI = GUI(wdBaseWindow)
+wdBaseWindow.mainloop()
+
+
+
+

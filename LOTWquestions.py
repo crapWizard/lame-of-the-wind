f3c962d532994ef801480e3a78d7ec5db5225e0c
crapWizard <hairlesschickens@gmail.com>
2018-02-24 13:52:21 +1100

    Add files via upload

---
+++ LOTWquestions.py
@@ -0,0 +1,13 @@
+questions:
+    true and false
+    lead is heavier than gold // false
+    obsidian black ink is prized by master archivists for its quck drying ability // false
+    the square root of pie exists // true
+    it is impossible to transform sounds energy into light energy // false
+    you can speak Siaru? // true
+    how many clouds are there // true
+    there are 206 bones in the human body // true
+    according to decarte, cognito egro sum? // true
+    a momment is a better link to a human person than wax mixed with blood // true
+
+    
